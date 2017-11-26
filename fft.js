3415b5267904fd92110e7c77e1d3d63ff02a6825
oujcddhdnc <34000859+oujcddhdnc@users.noreply.github.com>
2017-11-26 18:30:21 +0700

    Update fft.js

--- fft.js
+++ fft.js
@@ -12,8 +12,8 @@ const rp = require('request-promise');
 const _ = require('lodash');
 
 const User = {
-    username: '',
-    password: ''
+    username: 'lucu_zekali',
+    password: 'ayam12345'
 }
 
 const TargetUsername = '';
